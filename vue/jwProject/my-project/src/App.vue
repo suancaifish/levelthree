a05a44f349aeb61a8fdc3131b8001f1c469ff854
jw <jiawei316@qq.com>
2019-11-06 22:29:50 +0800

    改账户

--- vue/jwProject/my-project/src/App.vue
+++ vue/jwProject/my-project/src/App.vue
@@ -1,14 +1,11 @@
 <template>
-  <div id="app">
-    
-    <router-view />
-  </div>
+    <div id="app">
+        <router-view />
+    </div>
 </template>
 <script>
-
 </script>
 
 
 <style>
-
 </style>
